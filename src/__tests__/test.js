0dec611cc998a3197547922d21118d04144b7e9f
VoynovDev <vladimir.voynov2001@mail.ru>
2024-05-25 16:54:43 +0300

    fix eslint error

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,28 +1,33 @@
 import { checkHealth } from "../checkHealth.js";
-
+// eslint-disable-next-line
 test("Check Health Test1", () => {
 	let player = {
 		name: "Маг",
 		health: 90
 	}
 	const result = checkHealth(player);
+	// eslint-disable-next-line
 	expect(result).toBe("Healthy")
 })
 
+// eslint-disable-next-line
 test("Check Health Test2", () => {
 	let player = {
 		name: "Маг",
 		health: 45
 	}
 	const result = checkHealth(player);
+	// eslint-disable-next-line
 	expect(result).toBe("Wounded")
 })
 
+// eslint-disable-next-line
 test("Check Health Test3", () => {
 	let player = {
 		name: "Маг",
 		health: 7
 	}
 	const result = checkHealth(player);
+	// eslint-disable-next-line
 	expect(result).toBe("Critical")
 })
